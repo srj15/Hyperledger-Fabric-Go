29171f2ba941dc3bf3c91fc8b0c5de768b343bf2
s0g0com <sooraj.ganesh@walmart.com>
2023-06-13 08:54:00 +0530

    Updated

--- chaincode-go/DisputeNegationSystem.go
+++ chaincode-go/DisputeNegationSystem.go
@@ -8,7 +8,7 @@ import (
 	"log"
 
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
-	"github.com/hyperledger/fabric-samples/asset-transfer-basic/chaincode-go/chaincode"
+	"github.com/srj15/Hyperledger-Fabric-Go/chaincode-go/chaincode"
 	// "gecgithub01.walmart.com/s0g0com/Hyperledger-DisputeNegationSystem/chaincode-go/chaincode"
 )
 
